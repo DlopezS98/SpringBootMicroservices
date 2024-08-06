18b2d9ba3b6a68c80e6cffb6bfcba78b6f449bb2
DlopezS98 <01dlopezs98@gmail.com>
2024-08-05 23:34:16 -0600

    Add Storyshelf micro-service - initial setup

---
+++ ms-writer/src/main/java/net/storyshelf/ms_writer/MsWriterApplication.java
@@ -0,0 +1,13 @@
+package net.storyshelf.ms_writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsWriterApplication.class, args);
+	}
+
+}
---
+++ ms-writer/src/test/java/net/storyshelf/ms_writer/MsWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package net.storyshelf.ms_writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
