1e9603e0a4bf887d04891c2a67ceab72866a6f07
DlopezS98 <01dlopezs98@gmail.com>
2024-08-23 23:20:37 -0600

    Enable JPA Repositories

--- ms-writer/src/main/java/net/storyshelf/ms_writer/MsWriterApplication.java
+++ ms-writer/src/main/java/net/storyshelf/ms_writer/MsWriterApplication.java
@@ -2,11 +2,13 @@ package net.storyshelf.ms_writer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 //@EnableDiscoveryClient
+@EnableJpaRepositories(basePackages = "net.storyshelf.ms_writer.repositories")
 public class MsWriterApplication {
 
 	public static void main(String[] args) {
