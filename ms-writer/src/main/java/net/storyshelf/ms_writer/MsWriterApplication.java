cdd2bf91e7ddeb7ae9ee0e21797b00bc0f0d7886
DlopezS98 <01dlopezs98@gmail.com>
2024-08-24 00:23:35 -0600

    Add bean for rest template (http client service)

--- ms-writer/src/main/java/net/storyshelf/ms_writer/MsWriterApplication.java
+++ ms-writer/src/main/java/net/storyshelf/ms_writer/MsWriterApplication.java
@@ -2,8 +2,11 @@ package net.storyshelf.ms_writer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
@@ -13,5 +16,11 @@ public class MsWriterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsWriterApplication.class, args);
+	}
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
