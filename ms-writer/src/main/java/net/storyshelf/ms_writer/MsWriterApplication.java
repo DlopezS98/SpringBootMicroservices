4c4e20a2fe7bfc4986da5404dad61a16dc10904c
DlopezS98 <01dlopezs98@gmail.com>
2024-08-25 10:33:21 -0600

    Change to JPA repositories

--- ms-writer/src/main/java/net/storyshelf/ms_writer/MsWriterApplication.java
+++ ms-writer/src/main/java/net/storyshelf/ms_writer/MsWriterApplication.java
@@ -4,14 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+// import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 //@EnableDiscoveryClient
-@EnableJpaRepositories(basePackages = "net.storyshelf.ms_writer.repositories")
+// @EnableJpaRepositories(basePackages = "net.storyshelf.ms_writer.repositories")
 public class MsWriterApplication {
 
 	public static void main(String[] args) {
--- ms-writer/src/main/java/net/storyshelf/ms_writer/repositories/WishListItemRepository.java
+++ ms-writer/src/main/java/net/storyshelf/ms_writer/repositories/WishListItemRepository.java
@@ -4,10 +4,11 @@ import net.storyshelf.ms_writer.entities.WishListItem;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WishListItemRepository extends CrudRepository<WishListItem, String> {
+public interface WishListItemRepository extends JpaRepository<WishListItem, String> {
     Optional<WishListItem> findByBookId(String bookId);
 }
